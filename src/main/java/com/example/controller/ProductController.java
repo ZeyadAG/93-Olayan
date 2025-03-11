6b250da5da2fe09fada62481cb8b2b15c69afd9e
Ali Badawi <alibadawi2002@hotmail.com>
2025-03-11 17:18:47 +0200

    finished controllers

---
+++ src/main/java/com/example/controller/ProductController.java
@@ -0,0 +1,52 @@
+package com.example.controller;
+
+import com.example.model.Product;
+import com.example.service.ProductService;
+import org.springframework.web.bind.annotation.*;
+import java.util.*;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping("/")
+    public Product addProduct(@RequestBody Product product) {
+        return productService.addProduct(product);
+    }
+
+    @GetMapping("/")
+    public ArrayList<Product> getProducts() {
+        return productService.getProducts();
+    }
+
+    @GetMapping("/{productId}")
+    public Product getProductById(@PathVariable UUID productId) {
+        return productService.getProductById(productId);
+    }
+
+    @PutMapping("/update/{productId}")
+    public Product updateProduct(@PathVariable UUID productId, @RequestBody Map<String, Object> body) {
+        String newName = (String) body.get("newName");
+        double newPrice = Double.parseDouble(body.get("newPrice").toString());
+        return productService.updateProduct(productId, newName, newPrice);
+    }
+
+
+    @PutMapping("/applyDiscount")
+    public String applyDiscount(@RequestParam double discount, @RequestBody ArrayList<UUID> productIds) {
+        productService.applyDiscount(discount, productIds);
+        return "Discount applied successfully";
+    }
+
+    @DeleteMapping("/delete/{productId}")
+    public String deleteProductById(@PathVariable UUID productId) {
+        productService.deleteProductById(productId);
+        return "Product deleted successfully";
+    }
+}
---
+++ src/main/java/com/example/controller/UserController.java
@@ -0,0 +1,81 @@
+package com.example.controller;
+
+import com.example.model.Order;
+import com.example.model.Product;
+import com.example.model.User;
+import com.example.service.CartService;
+import com.example.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.*;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserService userService;
+    private final CartService cartService;
+
+    public UserController(UserService userService, CartService cartService) {
+        this.userService = userService;
+        this.cartService = cartService;
+    }
+
+    @PostMapping("/")
+    public User addUser(@RequestBody User user) {
+        return userService.addUser(user);
+    }
+
+    @GetMapping("/")
+    public ArrayList<User> getUsers() {
+        return userService.getUsers();
+    }
+
+    @GetMapping("/{userId}")
+    public User getUserById(@PathVariable UUID userId) {
+        return userService.getUserById(userId);
+    }
+
+    @GetMapping("/{userId}/orders")
+    public List<Order> getOrdersByUserId(@PathVariable UUID userId) {
+        return userService.getOrdersByUserId(userId);
+    }
+
+    @PostMapping("/{userId}/checkout")
+    public ResponseEntity<String> addOrderToUser(@PathVariable UUID userId) {
+            userService.addOrderToUser(userId);
+            return ResponseEntity.ok("Order placed successfully");
+        }
+
+    @PostMapping("/{userId}/removeOrder")
+    public ResponseEntity<String> removeOrderFromUser(@PathVariable UUID userId, @RequestParam UUID orderId) {
+        userService.removeOrderFromUser(userId, orderId);
+        return ResponseEntity.ok("Order removed successfully.");
+    }
+
+    @DeleteMapping("/{userId}/emptyCart")
+    public ResponseEntity<String> emptyCart(@PathVariable UUID userId) {
+        userService.emptyCart(userId);
+        return ResponseEntity.ok("Cart emptied successfully.");
+    }
+
+    @PutMapping("/addProductToCart")
+    public ResponseEntity<String> addProductToCart(@RequestParam UUID userId, @RequestBody Product product) {
+        cartService.addProductToCart(userId, product);
+        return ResponseEntity.ok("Product added to cart successfully.");
+    }
+
+    @PutMapping("/deleteProductFromCart")
+    public ResponseEntity<String> deleteProductFromCart(@RequestParam UUID userId, @RequestBody Product product) {
+        cartService.deleteProductFromCart(userId, product);
+        return ResponseEntity.ok("Product removed from cart successfully.");
+    }
+
+    @DeleteMapping("/delete/{userId}")
+    public ResponseEntity<String> deleteUserById(@PathVariable UUID userId) {
+        userService.deleteUserById(userId);
+        return ResponseEntity.ok("User deleted successfully.");
+    }
+
+}
